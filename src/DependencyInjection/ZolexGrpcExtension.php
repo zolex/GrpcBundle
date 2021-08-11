2c3a40209b36269bd43ee6f319999f3160c12fba
Andreas Linden <andreas.linden@coxautoinc.com>
2021-08-11 06:15:04 +0200

    remove ClientFactory and instead register configured clients as symfony services. refactor gRPC-service registration from server runtime into container compilation.

--- src/DependencyInjection/ZolexGrpcExtension.php
+++ src/DependencyInjection/ZolexGrpcExtension.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Zolex\GrpcBundle\DependencyInjection;
 
+use Grpc\BaseStub;
+use Grpc\ChannelCredentials;
 use Spiral\GRPC\ServiceInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Zolex\GrpcBundle\GRPC\Exception;
 
 class ZolexGrpcExtension extends Extension implements CompilerPassInterface
 {
@@ -28,12 +33,6 @@ class ZolexGrpcExtension extends Extension implements CompilerPassInterface
 
         $configuration = $this->getConfiguration($configs, $container);
         $this->config = $this->processConfiguration($configuration, $configs);
-
-        $baseServer = $container->getDefinition('zolex.grpc.base_server');
-        $baseServer->setArgument(1, $this->config['server']['options']);
-
-        $clientFactory = $container->getDefinition('zolex.grpc.client_factory');
-        $clientFactory->setArgument(0, $this->config['clients']);
     }
 
     /**
@@ -41,13 +40,62 @@ class ZolexGrpcExtension extends Extension implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        foreach ($container->getDefinitions() as $definition) {
+        $server = $container->getDefinition('zolex.grpc.base_server');
+        foreach ($container->getDefinitions() as $id => $definition) {
             $class = $definition->getClass();
-            if (null !== $class && class_exists($class, false)
-                && ($interfaces = class_implements($class))
-                && isset($interfaces[ServiceInterface::class])) {
-                $definition->addTag('zolex.grpc.service');
+            if (null === $class || !class_exists($class, false)) {
+                continue;
             }
+
+            // add grpc-clients as symfony services in the DI container
+            if (is_subclass_of($class, BaseStub::class)) {
+                $config = $this->config['clients'][$class] ?? $this->config['clients']['default'] ?? null;
+                if (null === $config) {
+                    $container->removeDefinition($id);
+                    trigger_error(sprintf('No gRPC client config found for "%s" and no default config exists.', $class), E_USER_WARNING);
+                    continue;
+                }
+
+                $credentials = new Definition(ChannelCredentials::class);
+                if ($config['secure'] ?? false) {
+                    $credentials->setFactory([ChannelCredentials::class, 'createSsl'], [/* TODO: certs params */]);
+                } else {
+                    $credentials->setFactory([ChannelCredentials::class, 'createInsecure']);
+                }
+                $credentialsServiceId = $class . '_credentials';
+                $container->setDefinition($credentialsServiceId, $credentials);
+
+                $definition->addTag('zolex.grpc.client');
+                $definition->setArgument(0, sprintf("%s:%d", $config['host'], $config['port']));
+                $definition->setArgument(1, [
+                    'credentials' => new Reference($credentialsServiceId),
+                ]);
+
+            // register grpc-services in the grpc base server
+            } else if (($interfaces = class_implements($class)) && isset($interfaces[ServiceInterface::class])) {
+                if (true === $this->config['server']['enabled']) {
+                    $definition->addTag('zolex.grpc.service');
+                    foreach ($interfaces as $interface) {
+                        if (is_subclass_of($interface, ServiceInterface::class)) {
+                            $server->addMethodCall('registerService', [
+                                $interface,
+                                $definition,
+                            ]);
+                            break;
+                        }
+                    }
+                } else {
+                    $container->removeDefinition($id);
+                }
+            }
+        }
+
+        if (true === $this->config['server']['enabled']) {
+            $baseServer = $container->getDefinition('zolex.grpc.base_server');
+            $baseServer->setArgument(1, $this->config['server']['options']);
+        } else {
+            $container->removeDefinition('zolex.grpc.base_server');
+            $container->removeDefinition('zolex.grpc.server');
         }
     }
 }
--- src/GRPC/Server.php
+++ src/GRPC/Server.php
@@ -5,47 +5,18 @@ declare(strict_types=1);
 namespace Zolex\GrpcBundle\GRPC;
 
 use Spiral\GRPC\Server as BaseServer;
-use Spiral\GRPC\ServiceInterface;
 use Spiral\RoadRunner\Worker;
 
 class Server
 {
-    /**
-     * @var iterable<ServiceInterface> $services
-     */
     public function __construct(
         private BaseServer $server,
         private Worker $worker,
-        private iterable $services
     ) {
     }
 
-    /**
-     * gRPC services must be rigistered with their generated interfaces.
-     * Automatically picks the right interface for registration.
-     *
-     * @throws \ReflectionException
-     */
-    private function registerServices()
-    {
-        foreach ($this->services as $service) {
-            $reflection = new \ReflectionClass($service);
-            $interfaces = $reflection->getInterfaceNames();
-            foreach ($interfaces as $interface) {
-                if (is_subclass_of($interface, ServiceInterface::class)) {
-                    $this->server->registerService($interface, $service);
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * @throws \ReflectionException
-     */
     public function serve()
     {
-        $this->registerServices();
         $this->server->serve($this->worker);
     }
 }
